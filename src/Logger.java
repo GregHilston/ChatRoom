b58dc5d3ceb0cb062c1acaf374805e2eabef31f5
Nicole <ngager23@gmail.com>
2014-08-13 17:51:53 -0400

    Initial Logger commit. Wrote createLogFile() and writeMessage()

---
+++ src/Logger.java
@@ -0,0 +1,82 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+
+/***
+ * Create a log file called [mm-dd-yyyy].log in the folder logs
+ */
+
+public class Logger {
+    private static File dir = new File("logs");
+    private static File file = new File(getFileName());
+    private static File fullPathToNewFile = new File(dir.toString() + "/" + file.toString());
+
+
+    /***
+     * Writes the message to the log file, prefaced with the current time
+     *
+     * @param message   the message to be printed to the log file
+     */
+    static void writeMessage( String message ) {
+
+        // append to the end of the file (time): [message]
+        try {
+            FileWriter fileWritter = new FileWriter(fullPathToNewFile, true);
+            BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+            bufferWritter.write(message + "\n");
+            bufferWritter.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    /***
+     * Creates the "Logs" directory if does not already exist and creates the log file for this server's lifetime.
+     * If the server already ran on this day, the new logfile will just append to the first one.
+     */
+    public static void createLogFile(String message) {
+        // If directory doesn't exist, then create it
+        if(!dir.exists()) {
+            dir.mkdir();
+        }
+
+
+        // If file doesn't exist, then create it
+        if(!fullPathToNewFile.exists()){
+            try {
+                fullPathToNewFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+        Logger.writeMessage(message);
+    }
+
+
+    /***
+     * Returns the filename to be used for this Server's log file
+     *
+     * @return  filename
+     */
+    public static String getFileName() {
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH); // Note: zero based!
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+        return month + "_" + day + "_" + year + ".log";
+    }
+
+
+    public static void main(String[] args) {
+        // testing
+        Logger.createLogFile("test.log");
+        Logger.writeMessage("Hello World");
+    }
+}
